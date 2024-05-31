7dfa0b334d2fd073d02fb761b2d08d50d7af8121
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2024-05-31 21:30:23 +0800

    setup express js server

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-//import 
+// import 
 require('dotenv').config();
 const mongoose = require('mongoose');
+const express = require('express');
 
-//connect to database
+const app = express();
+
+// connect to database
 const dbURI = process.env.DB_URI;
+
 mongoose.connect(dbURI)
     .then(result => console.log('connected to:', result.connection.name))
     .catch(err => console.log(err));
+
+
+
+// set ejs as view engine
+app.set('view engine', "ejs");
+
+// setup express server 
+app.listen(3000, () => {
+    console.log('server is running..');
+});
+
+
+console.log('je;;;');
